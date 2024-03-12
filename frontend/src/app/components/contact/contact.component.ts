84f5e7d246d9fa5bd952c1d8cf51f2630c68e7b5
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-03-12 10:14:30 +0000

    HU-18 Created contact page

--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -1,17 +1,38 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    FloatLabelModule
+  ],
   templateUrl: './contact.component.html',
   standalone: true,
   styleUrl: './contact.component.css'
 })
 export class ContactComponent implements OnInit{
 
-  constructor() {}
+  contactForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
+  private initializeForm() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      surname: [''],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+      privacy: [false, Validators.requiredTrue]
+    });
+  }
 }
