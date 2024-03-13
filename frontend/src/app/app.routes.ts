d02ed23672cd677519db0129069341a10eb1fbe5
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-03-13 12:04:43 +0000

    HU-10 Added home to path routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import {ContactComponent} from "./components/contact/contact.component";
+import {HomeComponent} from "./components/home/home.component";
 
 export const routes: Routes = [
   {
     path: "contacto",
     component: ContactComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
   }
 ];
