9dcce7cea09c7f1966213f726c5a1b217e269f60
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-03-14 22:28:47 +0000

    HU-18 WIP

---
+++ frontend/src/app/components/contact-messages/contact-messages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-messages',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-messages.component.html',
+  styleUrl: './contact-messages.component.scss'
+})
+export class ContactMessagesComponent {
+
+}
