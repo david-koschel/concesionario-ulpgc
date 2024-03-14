5a51f650a5c0190dedd4b5fd80d5a54965cd8bb1
AcoidanRodríguez <acoidanrp13@gmail.com>
2024-03-14 17:15:57 +0000

    Path to catalogue component added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {ContactComponent} from "./components/contact/contact.component";
 import {HomeComponent} from "./components/home/home.component";
+import { CatalogueComponent } from './components/catalogue/catalogue.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: "home",
     component: HomeComponent
+  },
+  {
+    path: "catalogue",
+    component: CatalogueComponent
   },
   {
     path: "",
