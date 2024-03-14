36a0edf98f1f47c5e6c1ff6bcc86b72540c67714
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-03-14 12:54:25 +0000

    HU-18 Created backend entity and service for contact messages

---
+++ backend/src/main/java/ps/backend/entity/ContactMessage.java
@@ -0,0 +1,64 @@
+package ps.backend.entity;
+
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+public class ContactMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String subject;
+
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String message;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ backend/src/main/java/ps/backend/service/ContactMessageService.java
@@ -0,0 +1,31 @@
+package ps.backend.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ps.backend.entity.ContactMessage;
+import ps.backend.repository.ContactMessageRepository;
+
+import java.util.List;
+
+@Service
+public class ContactMessageService {
+    @Value("${EMAIL.IT}")
+    private String itEmail;
+    private final ContactMessageRepository contactMessageRepository;
+
+    public ContactMessageService(ContactMessageRepository contactMessageRepository){
+        this.contactMessageRepository = contactMessageRepository;
+    }
+
+    public List<ContactMessage> findAll(){
+        return contactMessageRepository.findAll();
+    }
+
+    public void save(ContactMessage contactMessage){
+        contactMessageRepository.save(contactMessage);
+    }
+
+    public void sendContactMessageEmail(ContactMessage contactMessage){
+        this.save(contactMessage);
+    }
+}
