ffd88e45648461d2500d38b2a18c632c48abf7c7
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-04-10 10:11:02 +0100

    HU-19 Created database migrations

---
+++ backend/src/main/resources/db/migration/V4__car_model.sql
@@ -0,0 +1,4 @@
+create table car_model (
+    id int primary key auto_increment,
+    model_name varchar(255) not null
+);
---
+++ backend/src/main/resources/db/migration/V5__test_drive.sql
@@ -0,0 +1,12 @@
+create table test_drive_car (
+    id int primary key auto_increment,
+    car_model_id int not null
+);
+
+create table test_drive_request (
+    id int primary key auto_increment,
+    test_drive_car_id int not null,
+    startDate datetime not null,
+    endDate datetime not null,
+    user_id int not null
+);
