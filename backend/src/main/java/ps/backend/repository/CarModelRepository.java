6638c37a825196351e5682c6722c162d4ae32a36
Pablo Santana <pablo.santana116@alu.ulpgc.es>
2024-04-19 13:07:55 +0100

    HU-19 Deleted CarModel entity

--- backend/src/main/java/ps/backend/repository/CarModelRepository.java
+++
@@ -1,9 +0,0 @@
-package ps.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ps.backend.entity.CarModel;
-
-@Repository
-public interface CarModelRepository extends JpaRepository<CarModel, Integer> {
-}
--- backend/src/main/resources/db/migration/V4__car_model.sql
+++
@@ -1,4 +0,0 @@
-create table car_model (
-    id int primary key auto_increment,
-    model_name varchar(255) not null
-);
