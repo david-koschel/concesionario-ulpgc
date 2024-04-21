f7070311ee429882ede92b255c15ad8c2ff2ed1d
quecur <queolicar@gmail.com>
2024-04-21 22:15:28 +0200

    privacida y cookies y terminos y condiciones

---
+++ frontend/src/app/components/termsandconditions/termsandconditions.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TermsandconditionsComponent } from './termsandconditions.component';
+
+describe('TermsandconditionsComponent', () => {
+  let component: TermsandconditionsComponent;
+  let fixture: ComponentFixture<TermsandconditionsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TermsandconditionsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TermsandconditionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/components/termsandconditions/termsandconditions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termsandconditions',
+  standalone: true,
+  imports: [],
+  templateUrl: './termsandconditions.component.html',
+  styleUrl: './termsandconditions.component.scss'
+})
+export class TermsandconditionsComponent {
+
+}
