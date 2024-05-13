98cb7964bed33850ad7f33143bd533abf2c4637f
david-koschel <113340344+david-koschel@users.noreply.github.com>
2024-05-14 00:14:18 +0100

    Working on fixing bugs

--- frontend/src/app/components/rent-date/rent-date.component.ts
+++ frontend/src/app/components/rent-date/rent-date.component.ts
@@ -1,25 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormControl } from '@angular/forms';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {CalendarModule} from "primeng/calendar";
 
 
 @Component({
   selector: 'app-rent-date',
   standalone: true,
   imports: [
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MatFormFieldModule,
-    FormsModule, 
+    FormsModule,
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    CalendarModule
   ],
   templateUrl: './rent-date.component.html',
   styleUrl: './rent-date.component.scss'
@@ -28,6 +22,7 @@ export class RentDateComponent {
   fechaRecogida = new FormControl();
   fechaLlegada = new FormControl();
   fechaInvalida = false;
+  currentDate = new Date();
   @Output() buscarVehiculosDisponibles = new EventEmitter<{ fechaRecogida: Date, fechaLlegada: Date }>();
 
   onBuscarVehiculosDisponibles() {
@@ -37,16 +32,23 @@ export class RentDateComponent {
       setTimeout(() => {
         this.fechaInvalida = false;
       }, 3000);
-  
+
       return;
     }
-  
+
     this.fechaInvalida = false;
     this.buscarVehiculosDisponibles.emit({
       fechaRecogida: this.fechaRecogida.value,
       fechaLlegada: this.fechaLlegada.value
     });
   }
-  
-  
+
+  getDateClass(date: any) {
+    const calendarDate = new Date(date.year, date.month, date.day).toDateString();
+    /*if (this.occupiedDates.has(calendarDate)) {
+      return "occupied";
+    }*/
+    return null;
+  }
+
 }
